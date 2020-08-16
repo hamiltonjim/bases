22ca2e9e278d36cd11d90bfd72b6c4a74ad13e6d
Jim Hamilton <jim.hamilton@agilysys.com>
2020-08-16 13:38:08 -0700

    added options for (un)signed and 64 vs 32 bits

---
+++ README.md
@@ -0,0 +1,9 @@
+# bases -- convert input values to decimal, octal, hex, bytes, and UTF-8
+
+Converts any values on the command line (after options).
+
+##Options:
++ -s	Treat decimal values as signed (hex and octal as unsigned) (default)
++ -u	Treat all values as unsigned.
++ -l	Treat values as 64-bit (default).
++ -i	Treat values as 32-bit.
--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include <iomanip>
 #include <cstdlib>
 #include <string>
+#include <unistd.h>
 
 using namespace std;
+
+bool isSigned = true;
+bool bits64 = true;
 
 template <class T>
 void swapv(T& left, T& right) {
@@ -53,20 +57,64 @@ string bytes(long long number) {
     return bytes;
 }
 
+void showUsage() {
+    cerr << "Usage:" << endl
+    << "bases [-usil] number ..." << endl
+    << "\t-u or -s: treat as unsigned or signed (default signed)" << endl
+    << "\t-i or -l: treat as 32-bit or 64-bit (default 64-bit)" << endl;
+    
+    cerr << "Use -- to end options if you use negative numbers." << endl;
+}
+
 int main (int argc, char * const argv[]) {
     
+    // options
+    int optionVal;
+    while ((optionVal = getopt(argc, argv, "suli")) != -1) {
+        switch (optionVal) {
+            case 's':
+                isSigned = true;
+                break;
+                
+            case 'u':
+                isSigned = false;
+                
+            case 'l':
+                bits64 = true;
+                
+            case 'i':
+                bits64 = false;
+                
+            default:
+                showUsage();
+                return 1;
+                
+        }
+    }
     
-    for (int ctr = 1; ctr < argc; ++ctr) {
+    for (int ctr = optind; ctr < argc; ++ctr) {
         long long number;
         
-        if (isdigit(argv[ctr][0])) {
-            number = strtoll(argv[ctr], NULL, 0);
+        char * const nextArg = argv[ctr];
+        if (isdigit(nextArg[0]) || (nextArg[0] == '-' && isdigit(nextArg[1]))) {
+            if (bits64) {
+                if (isSigned) {
+                    number = strtoll(nextArg, NULL, 0);
+                } else {
+                    number = strtoull(nextArg, NULL, 0);
+                }
+            } else {
+                if (isSigned) {
+                    number = strtol(nextArg, NULL, 0);
+                } else {
+                    number = strtoul(nextArg, NULL, 0);
+                }
+            }
         } else {
             number = 0;
-            char *parm = argv[ctr];
-            for (int len = 0; len < 8 && parm[len]; ++len) {
+            for (int len = 0; len < 8 && nextArg[len]; ++len) {
                 number <<= 8;
-                number += parm[len];
+                number += nextArg[len];
             }
         }
         
